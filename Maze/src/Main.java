68c96340ffa6f99539bc1719e6ed1d0c785bb16e
Nate Tranel <njtranel@gmail.com>
2016-03-02 17:19:04 -0700

    starting to work

--- Maze/src/Main.java
+++ Maze/src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 	public static void main(String[] args) {
 		Maze m = new Maze();
-		m.traverse(0, 3, 0, 4);
+		m.traverse(0, 2, 0, 3);
 	}
 }
--- Maze/src/Maze.java
+++ Maze/src/Maze.java
@@ -13,14 +13,44 @@ public class Maze {
 			{'#','.','.','.','.','.','.','#','.','.','.','#'},
 			{'#','#','#','#','#','#','#','#','#','#','#','#'}};
 	public Maze() {
+		print();
+	}
+	
+	public void traverse(int x, int y, int xH, int yH) {
+		maze[y][x] = 'X';
+		print();
+		if (maze[y][x + 1] == '.' || maze[y][x + 1] == 'X') {
+			traverse(x + 1, y, xH + 1, yH);
+		}
+		else if (maze[y][x + 1] == '#') {
+			if (maze[yH][xH] == '.' || maze[yH][xH] == 'X') {
+				traverse(x, y + 1, xH - 1, yH);
+			}
+			else if (maze[yH][xH] == '#') {
+				if (maze[y - 1][x] == '.' || maze[y - 1][x] == 'X') {
+					traverse(x, y - 1, xH + 1, yH - 2);
+				}
+				else System.out.println("Got stuck at " + x + ", " + y);
+			}
+			else System.out.println("Got stuck at " + x + ", " + y);
+		}
+		else if (maze[y + 1][x] == '.' || maze[y + 1][x] == 'X') {
+			traverse(x, y + 1, xH, yH + 1);
+		}
+		else System.out.println("Got stuck at " + x + ", " + y);
+	}
+	
+	public void read() {
+		
+	}
+	
+	public void print() {
 		for (int i = 0; i < maze.length; i++) {
 			for (int j = 0; j < maze[0].length; j++) {
 				System.out.print(maze[i][j]);
 			}
 			System.out.println();
 		}
-	}
-	public void traverse(int xLoc, int yLoc, int handLocationX, int handLocationY) {
-		
+		System.out.println();
 	}
 }
