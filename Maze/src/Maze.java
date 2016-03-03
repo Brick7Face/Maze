2f2a8e130ff9b60e93dbc16a4a2304aa8cea3d8e
Nate Tranel <njtranel@gmail.com>
2016-03-02 18:24:03 -0700

    right and up work

--- Maze/src/Maze.java
+++ Maze/src/Maze.java
@@ -19,20 +19,20 @@ public class Maze {
 	public void traverse(int x, int y, int xH, int yH) {						//moving through the maze
 		maze[y][x] = 'X';														//prints maze and current spot you are on
 		print();
-		if (maze[y][x] == maze[yH + 1][xH]) {									//FACING RIGHT
+		if (x == xH && y == yH - 1) {											//FACING RIGHT
 			if (maze[y][x + 1] == '.' || maze[y][x + 1] == 'X') {				//check if you can move forward
 				traverse(x + 1, y, xH + 1, yH);
 			}
 			else if (maze[y][x + 1] == '#') {									//if not, check if right hand spot is open
 				if (maze[yH][xH] == '.' || maze[yH][xH] == 'X') {				 
-					traverse(x, y + 1, xH - 1, yH + 1);							
+					traverse(x, y, xH - 1, yH - 1);								//if so, turn right and face down			
 				}
 				else if (maze[yH][xH] == '#') {									//if right hand is on wall
 					traverse(x, y, xH + 1, yH - 1);								//turn left, face up
 				}
 			}
 		}
-		else if (maze[y][x] == maze[yH][xH + 1]) {								//FACING UP
+		else if (x == xH - 1 && y == yH) {										//FACING UP
 				if (maze[y - 1][x] == '.' || maze[y - 1][x] == 'X') {			//move up if possible
 					traverse(x, y - 1, xH, yH - 1);
 				}
@@ -45,10 +45,10 @@ public class Maze {
 					}
 				}
 		}
-		else if (maze[y][x] == maze[yH - 1][xH]) {								//FACING LEFT
+		else if (x == xH && y == yH + 1) {										//FACING LEFT
 
 		}
-		else if (maze[y][x] == maze[yH][xH - 1]) {								//FACING DOWN
+		else if (x == xH + 1 && y == yH) {										//FACING DOWN
 
 		}
 	}
